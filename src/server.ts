d5e3902dd629babd2306f3092302d0e8577c815a
Felipe Eduardo <felipegcbarros@gmail.com>
2025-03-18 11:24:04 -0300

    :sparkles: adiconado filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -104,6 +104,34 @@ app.delete("/movies/:id", async (req, res) => {
     res.status(200).send();
 });
 
+app.get("/movies/:genreName", async (req, res) => {
+    // receber o nome do gênero pelo parâmetro da rota
+    // console.log(req.params.genreName);
+
+    // filtrar os filmes do banco pelo gênero
+    try{
+        const moviesFilteredByGenreName = await prisma.movie.findMany({
+            include: {
+                genres: true,
+                languages: true
+            },
+
+            where: {
+                genres: {
+                    name: {
+                        equals: req.params.genreName,
+                        mode: "insensitive"
+                    }
+                }
+            }
+        });
+        // retornar os filmes filtrados na resposta da rota
+        res.status(200).send(moviesFilteredByGenreName)
+    }catch(error) {
+        res.status(500).send({ message: "Falha ao filtrar filmes por gênero" })
+    }
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
